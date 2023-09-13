8852e190cd586a34f929106cb7c55728a738944a
harshilchandratre <harshilchandratre2002@gmail.com>
2023-09-14 04:52:59 +0530

    auth0 test-1

--- server.js
+++ server.js
@@ -3,6 +3,26 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, { cors: { origin: '*' } });
 
+const { auth } = require('express-openid-connect');
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: 'a long, randomly-generated string stored in env',
+  baseURL: 'https://ayokrishi-connect-test-2.onrender.com',
+  clientID: 'uUhiLclkrVFddM3s1qbtS6SwlWqnJb3U',
+  issuerBaseURL: 'https://dev-n13bzjhem3ppzfcj.us.auth0.com'
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+
 const users = {};
 
 // Serve static files from the 'public' directory
